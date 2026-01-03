1a4a2e0546f1f53e5b015fb9a718c9b3c0513459
diogo <1240632@isep.ipp.pt>
2026-01-03 02:49:09 +0000

    aa

--- project-template/Core/headers/domain/model/WaitingRoom.h
+++ project-template/Core/headers/domain/model/WaitingRoom.h
@@ -1,6 +1,36 @@
 #ifndef WAITINGROOM_H
 #define WAITINGROOM_H
-class WaitingRoom {
 
+#include <list>
+#include "Appointment.h"
+
+class WaitingRoom {
+private:
+    std::list<Appointment*> waitingList;
+
+public:
+    WaitingRoom() = default;
+
+    void addClient(Appointment* appointment) {
+        waitingList.push_back(appointment);
+    }
+
+    void removeClient(Appointment* appointment) {
+        waitingList.remove(appointment);
+    }
+
+    std::list<Appointment*> getWaitingList() const {
+        return waitingList;
+    }
+
+    bool hasUser(std::string snsNumber) const {
+        for (auto app : waitingList) {
+            if (app->getUser()->getSNSNumber() == snsNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
 };
+
 #endif
