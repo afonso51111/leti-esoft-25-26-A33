67facb877e98552d7b1ba35cf8eb37e5c9b1dbe2
afonso51111 <1240857@isep.ipp.pt>
2026-01-12 00:18:14 +0000

    .

--- project-template/cmake-build-debug/CMakeFiles/project-template.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for project-template.
-# This may be replaced when dependencies are built.
--- project-template/cmake-build-debug/cmake_install.cmake
+++ project-template/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/MSI PC/Documents/Projeto/leti-esoft-25-26-A33/project-template
+# Install script for directory: C:/Users/ribei/esoft/leti-esoft-25-26-A33/project-template
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,24 +34,30 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.3.3/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2025.1.2/bin/mingw/bin/objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/MSI PC/Documents/Projeto/leti-esoft-25-26-A33/project-template/cmake-build-debug/Core/cmake_install.cmake")
+  include("C:/Users/ribei/esoft/leti-esoft-25-26-A33/project-template/cmake-build-debug/Core/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/MSI PC/Documents/Projeto/leti-esoft-25-26-A33/project-template/cmake-build-debug/CoreTests/cmake_install.cmake")
+  include("C:/Users/ribei/esoft/leti-esoft-25-26-A33/project-template/cmake-build-debug/CoreTests/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/MSI PC/Documents/Projeto/leti-esoft-25-26-A33/project-template/cmake-build-debug/ConsoleApp/cmake_install.cmake")
+  include("C:/Users/ribei/esoft/leti-esoft-25-26-A33/project-template/cmake-build-debug/ConsoleApp/cmake_install.cmake")
 endif()
 
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "C:/Users/ribei/esoft/leti-esoft-25-26-A33/project-template/cmake-build-debug/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
@@ -65,8 +71,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/MSI PC/Documents/Projeto/leti-esoft-25-26-A33/project-template/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/ribei/esoft/leti-esoft-25-26-A33/project-template/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
